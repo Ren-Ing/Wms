ad61f70239956e8ff8dce93a513b91fbbb1cc7a2
N1neSun <917549681@qq.com>
2021-04-01 17:54:45 +0800

    4.1日修改

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,17 +4,19 @@ package com.example.demo.controller;
 import com.example.demo.dao.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController("")
 public class TestController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/test")
-    public User findById(@RequestParam("id")Integer id ){
-        return userService.findById(id);
+    @ResponseBody
+    @RequestMapping("/login")
+    public ModelAndView findById(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("test");
+        return mv;
     }
 }
--- src/main/resources/static/wms.sql
+++ src/main/resources/static/wms.sql
@@ -1,4 +1,4 @@
-create table user(
+create table wms_user(
                      id int not null primary key auto_increment,
                      username varchar(32) not null ,
                      password varchar(64) not null ,
